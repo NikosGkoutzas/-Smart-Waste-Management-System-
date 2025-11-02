1a21955c2b9506a84e2d430254dd047c6b6fdc8b
Nikos Gkoutzas <72218979+NikosGkoutzas@users.noreply.github.com>
2025-11-02 10:37:10 +0200

    Delete smart_waste_management_system directory

--- smart_waste_management_system/smart_waste_management_system/celery.py
+++
@@ -1,9 +0,0 @@
-# smart_waste_management_system/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart_waste_management_system.settings")
-
-app = Celery("smart_waste_management_system")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
